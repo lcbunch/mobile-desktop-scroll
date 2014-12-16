99e8101dfe405c42316eb0bb98af0b57c04d4060
Lisa Bunch <lisabunch6@gmail.com>
2014-12-15 21:58:06 -0700

    Update scrollbutton.js

--- scrollbutton.js
+++ scrollbutton.js
@@ -1,7 +1,9 @@
+//This code will make a click and hold scroll button that works on mobile and desktop devices
+
 var scrolling = false;
 
 jQuery(function($){
-    $("#slider1-forw").bind("touchstart mousedown", function(e){
+    $("#slider-forward").bind("touchstart mousedown", function(e){
     	e.stopPropagation(); e.preventDefault();
     	scrolling = true;
     	startScrolling($("#scroll"), "-=150px");
@@ -11,6 +13,31 @@ jQuery(function($){
 		    });
 		});
 
+		function startScrolling(obj, param)
+		{
+		    obj.animate({"left": param}, "fast", function(){
+		    	if (scrolling)
+		    	{
+		    		startScrolling(obj, param);
+		    	}
+		    });
+		}
+
+//This is for going backward
+var scrolling = false;
+
+jQuery(function($){
+    $("#slider-back").bind("touchstart mousedown", function(e){
+    	e.stopPropagation(); e.preventDefault();
+    	scrolling = true;
+    	if ($('#scroll').position().left < "0") {
+    	startScrolling($("#scroll"), "+=150px");
+    	};
+    }).bind("touchend mouseup", function(e){
+    	e.stopPropagation(); e.preventDefault();
+    	scrolling = false;
+		    });
+	});
 		function startScrolling(obj, param)
 		{
 		    obj.animate({"left": param}, "fast", function(){
